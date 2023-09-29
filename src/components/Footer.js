98be7dc4b6bfb50cc10db6b7b71e6c800fd0e202
Abilash.D <115686328+Abilash1503@users.noreply.github.com>
2023-09-29 18:36:15 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import styles from "styles/Footer.module.scss";
+import GetIcon from "components/GetIcon";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <GetIcon icon="BsFillHeartFill" size={22} color="#da0037" /> <a href="https://github.com/Abilash1503">Abilash</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
